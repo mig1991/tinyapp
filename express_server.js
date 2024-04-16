fc92fa6baa4187b89933f9db26eb309001976297
Miguel Augusto <miguelmasche@gmail.com>
2024-04-15 19:50:51 -0700

    added GET and POST routes to express_server.js

--- express_server.js
+++ express_server.js
@@ -2,19 +2,45 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+function generateUrlSafeRandomString() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   v8xskF: "http://www.youtube.com",
-  g5fCvS: "http://www.facebook.com"
+  g5fCvS: "http://www.facebook.com",
 };
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
