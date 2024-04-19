df04fb1cd8e297e9aa6436468e88547c14cae80f
Miguel Augusto <miguelmasche@gmail.com>
2024-04-18 20:19:15 -0700

    deleted old script test in json

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { findUserViaEmail } = require('../helpers.js');
+const { findUserViaEmail: findUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -17,13 +17,13 @@ const testUsers = {
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = findUserViaEmail("user@example.com", testUsers);
+    const user = findUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.equal(user.id, expectedUserID, "The user ID should match the expected user ID.");
   });
 
   it('should return null for a non-existent email', function() {
-    const user = findUserViaEmail("doesntexist@yahoo.com", testUsers);
+    const user = findUserByEmail("doesntexist@yahoo.com", testUsers);
     assert.isNull(user, "no user should be returned for a non-existent email.");
   });
 });
